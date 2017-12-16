d8897559c1ae89a5a18ed8e0542ff130f3fbd9c8
James Miller <bensie@gmail.com>
2017-12-15 22:00:58 -0800

    promises

--- lambda/SetupCustomDomain/3_SESDomainCreator/index.js
+++ lambda/SetupCustomDomain/3_SESDomainCreator/index.js
@@ -4,102 +4,155 @@ const dynamodb = new AWS.DynamoDB()
 const route53 = new AWS.Route53()
 const ses = new AWS.SES()
 
-exports.handler = (event, context, callback) => {
-  const { domainName } = event
+class SkipToEndWithSuccessError extends Error {}
 
-  const getItemParams = {
-    TableName: process.env.DYNAMODB_TABLE,
-    Key: {
-      DomainName: {
-        S: domainName
+const getItemParams = domainName => {
+  return new Promise(resolve => {
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Key: {
+        DomainName: {
+          S: domainName
+        }
       }
     }
-  }
+    resolve(params)
+  })
+}
 
-  dynamodb.getItem(getItemParams, (err, data) => {
-    if (err) {
-      callback(err)
-      return
-    } else {
-      if (Object.keys(data).length === 0) {
-        callback(new Error('domain_name not found'))
-        return
+const getItem = itemParams => {
+  return new Promise((resolve, reject) => {
+    dynamodb.getItem(itemParams, (err, data) => {
+      if (err) {
+        reject(err)
+      } else if (Object.keys(data).length === 0) {
+        reject(new Error('domain_name not found'))
       } else {
-        const item = data.Item
-        if (item.SESDomainIdentityCreatedAt) {
-          callback(null, 'SES domain identity already exists, continuing')
-          return
-        }
-
-        const verifyDomainIdentityParams = {
-          Domain: item.DomainName.S
-        }
-        const verify = ses.verifyDomainIdentity(
-          verifyDomainIdentityParams,
-          (err, data) => {
-            if (err) {
-              callback(err)
-              return
-            } else {
-              const recordSetChanges = {
-                ChangeBatch: {
-                  Changes: [
-                    {
-                      Action: 'UPSERT',
-                      ResourceRecordSet: {
-                        Name: `_amazonses.${domainName}`,
-                        ResourceRecords: [
-                          {
-                            Value: `"${verify.VertificationToken}"`
-                          }
-                        ],
-                        TTL: 3600,
-                        Type: 'TXT'
-                      }
-                    }
-                  ],
-                  Comment: 'SES domain identity record'
-                },
-                HostedZoneId: item.Route53HostedZoneID.S
-              }
-              route53.changeResourceRecordSets(
-                recordSetChanges,
-                (err, data) => {
-                  if (err) {
-                    callback(err)
-                    return
-                  } else {
-                    const updateItemParams = {
-                      TableName: process.env.DYNAMODB_TABLE,
-                      Key: {
-                        DomainName: {
-                          S: domainName
-                        }
-                      },
-                      UpdateExpression:
-                        'SET SESDomainIdentityCreatedAt=:SESDomainIdentityCreatedAt',
-                      ExpressionAttributeValues: {
-                        ':SESDomainIdentityCreatedAt': {
-                          N: `${Date.now()}`
-                        }
-                      }
-                    }
-                    dynamodb.updateItem(updateItemParams, (err, data) => {
-                      if (err) {
-                        callback(err)
-                        return
-                      } else {
-                        callback(null, 'Success from SESDomainCreator')
-                        return
-                      }
-                    })
-                  }
-                }
-              )
-            }
-          }
-        )
+        resolve(data.Item)
       }
+    })
+  })
+}
+
+const getVerifyDomainIdentityParams = item => {
+  return new Promise(resolve => {
+    const params = {
+      Domain: item.DomainName.S
+    }
+    resolve({ item, params })
+  })
+}
+
+const verifyDomainIdentity = ({ item, verifyDomainIdentityParams }) => {
+  return new Promise((resolve, reject) => {
+    if (item.SESDomainIdentityCreatedAt) {
+      reject(
+        new SkipToEndWithSuccessError(
+          'SES domain identity already exists, continuing'
+        )
+      )
+    } else {
+      ses.verifyDomainIdentity(verifyDomainIdentityParams, (err, data) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve({ item, data })
+        }
+      })
     }
   })
+}
+
+const getRecordSetChanges = ({ item, domainIdentity }) => {
+  return new Promise(resolve => {
+    const changes = {
+      ChangeBatch: {
+        Changes: [
+          {
+            Action: 'UPSERT',
+            ResourceRecordSet: {
+              Name: `_amazonses.${item.DomainName.S}`,
+              ResourceRecords: [
+                {
+                  Value: `"${verify.VertificationToken}"`
+                }
+              ],
+              TTL: 3600,
+              Type: 'TXT'
+            }
+          }
+        ],
+        Comment: 'SES domain identity record'
+      },
+      HostedZoneId: item.Route53HostedZoneID.S
+    }
+  })
+}
+
+const changeResourceRecordSets = changes => {
+  return new Promise((resolve, reject) => {
+    route53.changeResourceRecordSets(recordSetChanges, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+const updateItemParams = ({ item }) => {
+  return new Promise(resolve => {
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Key: {
+        DomainName: {
+          S: item.DomainName.S
+        }
+      },
+      UpdateExpression:
+        'SET SESDomainIdentityCreatedAt=:SESDomainIdentityCreatedAt',
+      ExpressionAttributeValues: {
+        ':SESDomainIdentityCreatedAt': {
+          N: `${Date.now()}`
+        }
+      }
+    }
+    resolve({ item, params })
+  })
+}
+
+const updateItem = ({ item, updateItemParams }) => {
+  return new Promise((resolve, reject) => {
+    dynamodb.updateItem(updateItemParams, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+exports.handler = (event, context, callback) => {
+  const { domainName } = event
+  const success = () => callback(null, { domainName })
+  const failure = err => callback(err)
+
+  getItemParams()
+    .then(getItem)
+    .then(getVerifyDomainIdentityParams)
+    .then(verifyDomainIdentity)
+    .then(getRecordSetChanges)
+    .then(changeResourceRecordSets)
+    .then(updateItemParams)
+    .then(updateItem)
+    .then(() => success())
+    .catch(error => {
+      if (error instanceof SkipToEndWithSuccessError) {
+        success()
+      } else {
+        failure(error)
+      }
+    })
 }
