f8edd8d617b14dfcd1ecea656b973180eb552f66
James Miller <bensie@gmail.com>
2017-12-19 09:42:47 -0800

    use native-dns instead

--- lambda/SetupCustomDomain/2_DNSHostedZoneVerifier/index.js
+++ lambda/SetupCustomDomain/2_DNSHostedZoneVerifier/index.js
@@ -2,6 +2,7 @@ const AWS = require('aws-sdk')
 AWS.config.update({ region: process.env.AWS_REGION })
 const dynamodb = new AWS.DynamoDB()
 const dns = require('dns')
+const nativedns = require('native-dns')
 const parseDomain = require('parse-domain')
 
 class SkipToEndWithSuccessError extends Error {}
@@ -33,33 +34,78 @@ const fetchDomainRootNameservers = ({ item }) => {
     const domainInfo = parseDomain(item.DomainName.S)
     const registeredDomain = `${domainInfo.domain}.${domainInfo.tld}`
 
-    dns.setServers(['8.8.8.8', '8.8.4.4'])
-    dns.resolveNs(registeredDomain, (err, addresses) => {
+    const question = nativedns.Question({
+      name: registeredDomain,
+      type: 'NS'
+    })
+
+    const req = nativedns.Request({
+      question: question,
+      server: { address: '8.8.8.8', port: 53, type: 'udp' },
+      timeout: 1000
+    })
+
+    req.on('timeout', function() {
+      reject(new Error('Timeout making DNS lookup request'))
+    })
+
+    req.on('message', function(err, answer) {
       if (err) {
         reject(err)
       } else {
-        resolve({ item, domainRootNameservers: addresses })
+        resolve({
+          item,
+          domainRootNameservers: answer.answer.map(item => item.data)
+        })
+      }
+    })
+
+    req.send()
+  })
+}
+
+const fetchRootDomainNameserverIPs = ({ item, domainRootNameservers }) => {
+  return new Promise((resolve, reject) => {
+    dns.setServers(['8.8.8.8', '8.8.4.4'])
+    dns.resolve4(domainRootNameservers[0], (err, addresses) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ item, rootDomainNameserverIPs: addresses })
       }
     })
   })
 }
 
-const fetchTargetDomainNameservers = ({ item, domainRootNameservers }) => {
+const lookupTargetNameservers = ({ item, rootDomainNameserverIPs }) => {
   return new Promise((resolve, reject) => {
-    dns.resolve4(domainRootNameservers[0], (err, addresses) => {
+    const question = nativedns.Question({
+      name: item.DomainName.S,
+      type: 'NS'
+    })
+
+    const req = nativedns.Request({
+      question: question,
+      server: { address: rootDomainNameserverIPs[0], port: 53, type: 'udp' },
+      timeout: 1000
+    })
+
+    req.on('timeout', function() {
+      reject(new Error('Timeout making DNS lookup request'))
+    })
+
+    req.on('message', function(err, answer) {
       if (err) {
         reject(err)
       } else {
-        dns.setServers([addresses[0]])
-        dns.resolveNs(domainName, (err, addresses) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve({ item, targetDomainNameservers: addresses })
-          }
+        resolve({
+          item,
+          targetDomainNameservers: answer.authority.map(item => item.data)
         })
       }
     })
+
+    req.send()
   })
 }
 
@@ -126,7 +172,8 @@ exports.handler = (event, _context, callback) => {
 
   getItem(domainName)
     .then(fetchDomainRootNameservers)
-    .then(fetchTargetDomainNameservers)
+    .then(fetchRootDomainNameserverIPs)
+    .then(lookupTargetNameservers)
     .then(verifyExpectedNameserversMatch)
     .then(getUpdateItemParams)
     .then(updateItem)
