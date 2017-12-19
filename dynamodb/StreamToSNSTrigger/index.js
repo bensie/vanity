32f5f43b282d7f15e7ad045d43efae5151dbc77c
James Miller <bensie@gmail.com>
2017-12-18 21:51:00 -0800

    Only notify on insert/update

--- dynamodb/StreamToSNSTrigger/index.js
+++ dynamodb/StreamToSNSTrigger/index.js
@@ -47,23 +47,27 @@ const payloadObjectWithData = data => ({
 
 exports.handler = (event, context, callback) => {
   event.Records.forEach(record => {
-    const params = {
-      Message: JSON.stringify(payloadObjectWithData(record.dynamodb.NewImage)),
-      TopicArn: process.env.DYNAMODB_STREAM_SNS_TOPIC
-    }
-    sns.publish(params, function(err, data) {
-      if (err) {
-        console.error(
-          'Unable to send message. Error JSON:',
-          JSON.stringify(err, null, 2)
-        )
-      } else {
-        console.log(
-          'Results from sending message: ',
-          JSON.stringify(data, null, 2)
-        )
+    if (record.eventName == 'INSERT' || record.eventName == 'UPDATE') {
+      const params = {
+        Message: JSON.stringify(
+          payloadObjectWithData(record.dynamodb.NewImage)
+        ),
+        TopicArn: process.env.DYNAMODB_STREAM_SNS_TOPIC
       }
-    })
+      sns.publish(params, function(err, data) {
+        if (err) {
+          console.error(
+            'Unable to send message. Error JSON:',
+            JSON.stringify(err, null, 2)
+          )
+        } else {
+          console.log(
+            'Results from sending message: ',
+            JSON.stringify(data, null, 2)
+          )
+        }
+      })
+    }
   })
   callback(null, `Successfully processed ${event.Records.length} records.`)
 }
